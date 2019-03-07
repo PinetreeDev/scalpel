92518ec2f102176947fcb3fe520f0b888c130752
Justin Schaaf <justinhschaaf@outlook.com>
2019-03-06 18:22:23 -0800

    Initialize Stencil Project

---
+++ stencil.config.ts
@@ -0,0 +1,18 @@
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+
+export const config: Config = {
+  namespace: 'Scalpel',
+  plugins: [
+    sass()
+  ],
+  outputTargets:[
+    { type: 'dist' },
+    { type: 'docs' },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
+  ],
+  preamble: '(C) Pinetree Development https://pinetree.dev - Licensed under the MIT License',
+};
